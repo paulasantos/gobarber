5a4efc1e1a1a097f21c968525bb2495c6e5335a3
Paula Santos <paulafap@gmail.com>
2021-04-02 16:00:22 -0300

    nodemailer configuration and sending email notifying you of the cancellation of the appointment

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'b5525ef479dc50',
+    pass: 'b0bc7bdf36ad42',
+  },
+  default: {
+    from: 'Estudos projeto GoBarber <noreply@gobarber.com>',
+  },
+};

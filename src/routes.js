255b7cfeedacedc38e77e384a1881dd04e541f80
Paula Santos <paulafap@gmail.com>
2021-04-02 15:31:20 -0300

    cancellation of appointments and new controller to manage notifications

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentsController from './app/controllers/AppointmentsController';
 import ScheduleController from './app/controllers/ScheduleController';
+import NotificationController from './app/controllers/NotificationController';
 
 import authMiddeware from './app/middlewares/auth';
 
@@ -25,8 +26,11 @@ routes.get('/providers', ProviderController.index);
 
 routes.get('/appointments', AppointmentsController.index);
 routes.post('/appointments', AppointmentsController.store);
+routes.delete('/appointments/:id', AppointmentsController.delete);
 
 routes.get('/schedule', ScheduleController.index);
+routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
 
 routes.post('/files', uploads.single('file'), FileController.store);
 
